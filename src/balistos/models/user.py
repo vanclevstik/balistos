ec4b81524a2bd86b2089f22ae4b04de9ceddadfd
Vanč Levstik <vanc.levstik@gmail.com>
2014-02-08 19:30:15 +0100

    User, playlist, clip and other models added

---
+++ src/balistos/models/user.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""User model."""
+
+from pyramid_basemodel import Base
+from pyramid_basemodel import BaseMixin
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Unicode
+
+
+class User(Base, BaseMixin):
+    """A class representing a User."""
+
+    __tablename__ = 'users'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+    )
+    username = Column(
+        String,
+        unique=True,
+        nullable=False,
+    )
+    email = Column(
+        String,
+        unique=True,
+        nullable=True,
+    )
+    fullname = Column(
+        Unicode(200),
+        nullable=True,
+    )
+
+    @classmethod
+    def get_by_username(self, username):
+        """Get a User by username."""
+        result = User.query.filter_by(username=username)
+        if result.count() < 1:
+            return None
+
+        return result.one()
+
+    @classmethod
+    def get_all(class_, order_by='fullname', filter_by=None):
+        """Return all users.
+
+        filter_by: dict -> {'name': 'foo'}
+
+        By default, order by User.fullname.
+        """
+        User = class_
+        q = User.query
+        q = q.order_by(getattr(User, order_by))
+        if filter_by:
+            q = q.filter(filter_by)
+        return q
