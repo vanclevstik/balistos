e954198229c5a1ed32a884e0ff7ba9a7d565e249
Vanč Levstik <vanc.levstik@gmail.com>
2014-02-21 23:50:09 +0100

    Add limit to duration of videos added automatically

--- src/balistos/youtube.py
+++ src/balistos/youtube.py
@@ -7,6 +7,9 @@ import gdata.youtube.service
 from random import randrange
 
 
+MAX_SECONDS = 600
+
+
 def get_related_video(video_id):
     """
     Gets related video and its information
@@ -17,17 +20,34 @@ def get_related_video(video_id):
     :returns: all the info about related video
     :rtype:   dict of strings
     """
-
     yt_service = gdata.youtube.service.YouTubeService()
     yt_service.ssl = True
 
     yt_service.developer_key = DEVELOPER_KEY
 
-    related_feed = yt_service.GetYouTubeRelatedVideoFeed(video_id=video_id)
+    videos = None
+    while not videos:
+        related_feed = yt_service.GetYouTubeRelatedVideoFeed(video_id=video_id)
+        videos = select_video(related_feed.entry)
+        if not videos:
+            video_id = related_feed.entry[0].id.text.split('/')[-1]
 
-    videos = related_feed.entry
-    # videos_len = len(videos)
-    # end = videos_len > 10 and 10 or videos_len
+    return videos
+
+
+def select_video(videos):
+    """
+    Select videos from related videos list according to our options
+
+    :param    videos: list of videos
+    :type     videos: list of Youtube VideoEntry
+
+    :returns: metadata of selected video
+    :rtype:   dict
+    """
+    videos = [video for video in videos if int(video.media.duration.seconds) < MAX_SECONDS]  # noqa
+    if not videos:
+        return None
     video = videos[randrange(len(videos)-1)]
     title = video.title.text
     image_url = video.media.thumbnail[0].url
