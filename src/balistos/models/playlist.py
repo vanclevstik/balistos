251edf06d8307caeb97d7fb859e2bf5ffc05a0df
Vanč Levstik <vanc.levstik@gmail.com>
2014-02-23 03:01:20 +0100

    Modified models, added ChatMessage model

--- src/balistos/models/playlist.py
+++ src/balistos/models/playlist.py
@@ -130,3 +130,65 @@ class PlaylistUser(Base, BaseMixin):
             return []
 
         return result.all()
+
+
+class ChatMessage(Base, BaseMixin):
+
+    __tablename__ = 'chat_messages'
+
+    message = Column(
+        Unicode(200),
+        nullable=True,
+    )
+
+    playlist_id = Column(Integer, ForeignKey('playlists.id'))
+    playlist = relationship(
+        Playlist,
+        single_parent=True,
+        backref=backref(
+            'chats',
+            cascade='all, delete-orphan',
+            single_parent=False,
+            uselist=True,
+        )
+    )
+
+    posted = Column(
+        DateTime,
+        nullable=False,
+    )
+
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(
+        User,
+        single_parent=True,
+        backref=backref(
+            'chats',
+            cascade='all, delete-orphan',
+            single_parent=False,
+            uselist=True,
+        )
+    )
+
+    @classmethod
+    def get_by_playlist_and_user(self, playlist, user):
+        """Get ChatMessage by PlaylistClip and User."""
+        result = ChatMessage.query.filter(
+            ChatMessage.playlist == playlist,
+            ChatMessage.user == user
+        ).order_by('posted')
+        if result.count() < 1:
+            return None
+
+        return result.all()
+
+    @classmethod
+    def get_by_playlist(self, playlist):
+        """Get Chatmessages of playlist."""
+        result = ChatMessage.query.filter(
+            ChatMessage.playlist == playlist,
+        ).order_by('posted')
+        if result.count() < 1:
+            return []
+
+        return result.all()
